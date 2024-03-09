eb7f787ec5e2825c5bf107a05e250e8cca4e8d54
passl <30534845+pschropp@users.noreply.github.com>
2024-03-09 10:30:15 +0100

    Update arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,3 +1,5 @@
+//place next to .ino file, add '#include "arduino_secrets.h"' to that one and use the constants as named here
+
 //WIFI
 #define WIFI_SSID "your_wifi_ssid"
 #define WIFI_PASSWORD "your_wifi_password"
