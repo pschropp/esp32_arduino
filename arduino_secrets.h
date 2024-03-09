70ba432d3dac24a9584b104faa06c840a0a1a4ec
passl <30534845+pschropp@users.noreply.github.com>
2024-03-09 10:28:39 +0100

    Add files via upload

---
+++ arduino_secrets.h
@@ -0,0 +1,13 @@
+//WIFI
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
+
+//thinger.io
+#define USERNAME "your_user_name"
+#define DEVICE_ID "your_device_id"
+#define DEVICE_CREDENTIAL "your_device_credential"
+
+//thingspeak.com
+#define CHANNEL_ID 123456
+#define READ_APIKEY "your_write_api_key"
+#define WRITE_APIKEY "your_write_api_key"
